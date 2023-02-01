5ce546411d554c97128d7c196863776abccf8d03
Wesley Lima Guimarães <94397155+WesleyGuimaraes@users.noreply.github.com>
2023-01-31 22:37:15 -0300

    Add files via upload

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    //JSX  como o react trabalha com o visual
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner principal da pagina"/>
+        </header>
+    )
+}
+
+export default Banner
